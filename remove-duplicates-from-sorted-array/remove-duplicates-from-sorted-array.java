b66018db02fc29bcbf466faf6c1df3732b39fbfb
Biswa-Ranjan-Rout <90982608+Biswa-Ranjan-Rout@users.noreply.github.com>
2022-06-17 14:16:04 +0530

    Time: 1 ms, Memory: 47.8 MB - LeetHub

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,13 +1,15 @@
 class Solution {
 
     public int removeDuplicates(int[] nums) {
-        int i = 0;
-        for (int j = 1; j < nums.length; j++) {
+        int writePointer = 0;
+        for (int j = 1; j < nums.length; j++) { // j is used as a readPointer
             if (nums[j-1] != nums[j]) { // This means duplicate run has ended
-                nums[i + 1] = nums[j];
-                i++;
+                nums[writePointer + 1] = nums[j];
+                writePointer++;
             }
         }
-      return i+1; // As we are starting from 0.
+      return writePointer+1; // As we are starting from 0.
     }
 }
+
+// i is the index which holds only unique element
