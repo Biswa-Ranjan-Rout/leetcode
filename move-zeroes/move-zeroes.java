ea84a5b08aa599b30baf9fd2cae4e289f950afa9
Biswa-Ranjan-Rout <90982608+Biswa-Ranjan-Rout@users.noreply.github.com>
2022-06-17 14:42:33 +0530

    Time: 2 ms, Memory: 55.4 MB - LeetHub

---
+++ move-zeroes/move-zeroes.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if(nums == null){
+          return;
+        }
+        
+        int writeIndex = 0;
+        for(int j=0; j<nums.length; j++){
+          if(nums[writeIndex] == 0 && nums[j] != 0){
+            nums[writeIndex] = nums[j];
+            nums[j] = 0;
+            writeIndex++;
+          }
+          else if(nums[writeIndex] != 0){
+            writeIndex++;
+          }
+        }
+    }
+}
+//[1,0,1]
+
+//[1,0,1]
