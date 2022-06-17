45ef94b5f9da507e1570835251e9bc1d59ef04f6
Biswa-Ranjan-Rout <90982608+Biswa-Ranjan-Rout@users.noreply.github.com>
2022-06-17 15:04:43 +0530

    Time: 2 ms (75.00%), Space: 55.5 MB (5.69%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -5,15 +5,22 @@ class Solution {
             return;
         }
 
-        int writeIndex = 0;
-        for (int j = 0; j < nums.length; j++) {
-            if (nums[writeIndex] == 0 && nums[j] != 0) {
-                nums[writeIndex] = nums[j];
-                nums[j] = 0;
-                writeIndex++;
-            } else if (nums[writeIndex] != 0) {
-                writeIndex++;
+        int left = 0;
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != 0) {
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left++] = temp;
             }
         }
     }
 }
+// So the intuition is, we will have two pointers.
+// We will use swap technique.
+
+/* 
+- We will start both left and right pointers from 0th index.
+- If we will see a zero then we'll increment the right pointer but left pointer will remain same.
+- If we will encounter a non-zero value then we'll swap both left and right pointer value. And we'll increment left and right pointer.
+
+*/
