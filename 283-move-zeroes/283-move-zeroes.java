6c606fac22793c8adcd94933f637a177369399d3
Biswa-Ranjan-Rout <90982608+Biswa-Ranjan-Rout@users.noreply.github.com>
2022-06-17 14:51:49 +0530

    Time: 2 ms (75.00%), Space: 43.4 MB (95.87%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,19 +1,19 @@
 class Solution {
+
     public void moveZeroes(int[] nums) {
-        if(nums == null){
-          return;
+        if (nums == null) {
+            return;
         }
-        
+
         int writeIndex = 0;
-        for(int j=0; j<nums.length; j++){
-          if(nums[writeIndex] == 0 && nums[j] != 0){
-            nums[writeIndex] = nums[j];
-            nums[j] = 0;
-            writeIndex++;
-          }
-          else if(nums[writeIndex] != 0){
-            writeIndex++;
-          }
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[writeIndex] == 0 && nums[j] != 0) {
+                nums[writeIndex] = nums[j];
+                nums[j] = 0;
+                writeIndex++;
+            } else if (nums[writeIndex] != 0) {
+                writeIndex++;
+            }
         }
     }
 }
