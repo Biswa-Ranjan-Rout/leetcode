b010e5b6e181feb9cf4b1e260a507e1966b39664
Biswa-Ranjan-Rout <90982608+Biswa-Ranjan-Rout@users.noreply.github.com>
2022-06-18 15:32:02 +0530

    Time: 3 ms (28.36%), Space: 41.7 MB (60.58%) - LeetHub

--- 1051-height-checker/1051-height-checker.java
+++ 1051-height-checker/1051-height-checker.java
@@ -18,3 +18,8 @@ class Solution {
         return count;
     }
 }
+/* 
+  One way of solving this problem is by sorting a copy of our input array and comparing it with the original array.
+
+Every time we see a student in the original array not matching with the same position in the sorted array, we increment our minStudentsMove counter by 1.
+*/
