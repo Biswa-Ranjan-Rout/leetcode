4a287e209269b0dcb2f994af2bead31ed60d2dac
Biswa-Ranjan-Rout <90982608+Biswa-Ranjan-Rout@users.noreply.github.com>
2022-06-19 23:58:25 +0530

    Time: 3 ms (89.70%), Space: 43.9 MB (83.29%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -9,7 +9,7 @@ class Solution {
             if (nums[i] == 0) {
                 zeroCount++;
             }
-            while (zeroCount > k) { // If 0 count become more than 1
+            while (zeroCount > k) { // If 0 count become more than k
                 j++; // then, we'll increment j until we reach see another zero
                 if (nums[j] == 0) {
                     zeroCount--; // Once count will become 1, we'll exit this loop, now j will be at 1st zero location, which we can flip for next max search
