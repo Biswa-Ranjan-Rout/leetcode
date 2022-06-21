22fa23fdf83618e2e81026214dbf30d646fd885a
Biswa-Ranjan-Rout <90982608+Biswa-Ranjan-Rout@users.noreply.github.com>
2022-06-21 16:42:57 +0530

    Time: 11 ms, Memory: 55.6 MB - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,40 +1,11 @@
 class Solution {
-    public int[] sortedSquares(int[] nums) {
-        for(int i=0;i<nums.length;i++){
-          nums[i] = nums[i] * nums[i];
-        }
-        Arrays.sort(nums);
-      return nums;
-    }
-}
-
-/*
---------------------------------------------------------------
-Efficient Solution:
---------------------------------------------------------------
-class Solution {
 
     public int[] sortedSquares(int[] nums) {
         int n = nums.length;
-        int[] res = new int[n];
-        int left = 0, right = n - 1;
+        int[] ans = new int[n];
+        for (int i = 0; i < n; ++i) ans[i] = nums[i] * nums[i];
 
-        for (int i = n - 1; i >= 0; i--) {
-            int squareNum = 0;
-            if (Math.abs(nums[left]) < (Math.abs(nums[right]))) {
-                squareNum = nums[right--];
-            } else {
-                squareNum = nums[left++];
-            }
-            res[i] = squareNum * squareNum;
-        }
-        return res;
+        Arrays.sort(ans);
+        return ans;
     }
 }
---------------------------------------------------------------
-Since the array A is sorted, loosely speaking it has some negative elements with squares in decreasing order, then some non-negative elements with squares in increasing order.
-For example, with [-3, -2, -1, 4, 5, 6], we have the negative part [-3, -2, -1] with squares [9, 4, 1], and the positive part [4, 5, 6] with squares [16, 25, 36]. Our strategy is to iterate over the negative part in reverse, and the positive part in the forward direction.
-Algorithm
-We can use two pointers to read the positive and negative parts of the array - one pointer j in the positive direction, and another i in the negative direction.
-Now that we are reading two increasing arrays (the squares of the elements), we can merge these arrays together using a two-pointer technique.
-*/
