673d02ae410d5cf0203cfad40fe12fb89ec4f240
Biswa-Ranjan-Rout <90982608+Biswa-Ranjan-Rout@users.noreply.github.com>
2022-09-12 00:27:21 +0530

    Time: 1 ms (89.71%), Space: 43.2 MB (69.31%) - LeetHub

---
+++ 78-subsets/78-subsets.java
@@ -0,0 +1,20 @@
+class Solution {
+    
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList();
+        findSub(0, nums, new ArrayList(), nums.length, result);
+        
+        return result;
+    }
+    public void findSub(int i, int[] arr, List<Integer> list, int n, List<List<Integer>> result){
+        if(i == n){
+           result.add(new ArrayList(list));
+           return;
+        }       
+        list.add(arr[i]);
+        findSub(i+1, arr,list,n, result);
+        list.remove(list.size() - 1);
+        
+        findSub(i+1, arr,list,n, result);
+    }
+}
